188322f111fa33aabd603dce8de7b5477819db21
syon <syon.xiii@gmail.com>
2022-10-08 20:28:18 +0900

    :hash: 5 - Bezier connector

--- canvas.js
+++ canvas.js
@@ -1,6 +1,7 @@
 import Konva from 'konva'
 import { createCross } from './lib/Cross'
 import { createCyanSquare } from './lib/CyanSquare'
+import { createConnector } from './lib/Connector'
 
 export function draw() {
   const stage = new Konva.Stage({
@@ -25,6 +26,15 @@ export function draw() {
   const rect1 = createCyanSquare({ x: 40, y: 40 })
   layer.add(rect1)
 
-  const rect2 = createCyanSquare({ x: 250, y: 80 })
+  const rect2 = createCyanSquare({ x: 250, y: 120 })
   layer.add(rect2)
+
+  const rect3 = createCyanSquare({ x: 450, y: 20 })
+  layer.add(rect3)
+
+  const connector1 = createConnector(rect1, rect2)
+  layer.add(connector1)
+
+  const connector2 = createConnector(rect2, rect3)
+  layer.add(connector2)
 }
