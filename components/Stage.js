8fd4aa8314974edaf08aaf65c6c6498d7fd374f5
syon <syon.xiii@gmail.com>
2022-11-05 19:14:03 +0900

    :hash: 11 - Synchronized objects

--- components/Stage.js
+++ components/Stage.js
@@ -1,13 +1,32 @@
+import React from "react";
 import { Stage, Layer } from "react-konva";
-import { useRecoilValue } from "recoil";
 import { $blueprint } from "../store/blueprint";
 
 const StageComponent = () => {
-  const d = useRecoilValue($blueprint.getter.gData);
-  const boxes = $blueprint.helper.createAllBoxes(d);
+  const [root, setRoot] = React.useState({});
+
+  React.useEffect(() => {
+    $blueprint.helper.getRemoteData().then((data) => {
+      setRoot(data);
+    });
+  }, [setRoot]);
+
+  const onMoving = (arg) => {
+    const { boxes, binds } = root;
+    const pos = { x: arg.x, y: arg.y };
+    const newBoxes = { ...boxes, [arg.id]: pos };
+    setRoot({ boxes: newBoxes, binds });
+  };
+
+  const boxes = $blueprint.helper.createAllBoxes(root.boxes, onMoving);
+  const binds = $blueprint.helper.createAllWires(root);
+
   return (
     <Stage width={window.innerWidth} height={window.innerHeight} draggable>
-      <Layer>{boxes}</Layer>
+      <Layer>
+        {boxes}
+        {binds}
+      </Layer>
     </Stage>
   );
 };
--- store/blueprint.js
+++ store/blueprint.js
@@ -1,47 +1,45 @@
-import { atom, selector } from "recoil";
-import NodeBox from "../components/NodeBox";
-import sampledata from "./sampledata.json";
+import CyanBox from "../components/CyanBox";
+import Wire from "../components/Wire";
 
-const state = {
-  aData: atom({
-    key: "aData",
-    default: sampledata,
-  }),
+const initData = {
+  boxes: {
+    cb1: { x: 63, y: 216 },
+    cb2: { x: 217, y: 32 },
+    cb3: { x: 178, y: 128 },
+    cb4: { x: 443, y: 50 },
+    cb5: { x: 424, y: 219 },
+    cb6: { x: 533, y: 130 },
+  },
+  binds: [
+    { from: "cb1", to: "cb2" },
+    { from: "cb3", to: "cb4" },
+    { from: "cb5", to: "cb6" },
+    { from: "cb6", to: "cb1" },
+    { from: "cb2", to: "cb5" },
+  ],
 };
 
-let cacheMaster = {};
+const state = {};
 
-const getter = {
-  gData: selector({
-    key: "gData",
-    get: ({ get }) => {
-      return state.aData;
-    },
-  }),
-};
+const getter = {};
 
 const helper = {
-  createBox(o) {
-    const pos = o.pos || { x: 0, y: 0 };
-    return (
-      <NodeBox
-        x={pos.x}
-        y={pos.y}
-        id={o.id}
-        title={o.title}
-        pieces={o.pieces}
-        key={o.id}
-      />
-    );
+  async getRemoteData() {
+    return initData;
   },
-  createAllBoxes(structure) {
-    return Object.entries(structure)
-      .map(([k, v]) => {
-        return { id: k, ...v };
-      })
-      .map((obj) => {
-        return this.createBox(obj);
-      });
+  createAllBoxes(boxes, onMoving) {
+    if (!boxes) return null;
+    return Object.entries(boxes || {}).map(([id, pos]) => {
+      return <CyanBox id={id} key={id} {...pos} onMoving={onMoving} />;
+    });
+  },
+  createAllWires(root) {
+    if (!root || !root.binds) return null;
+    return root.binds.map((bd, i) => {
+      const sp = root.boxes[bd.from];
+      const ep = root.boxes[bd.to];
+      return <Wire key={i} sp={sp} ep={ep} />;
+    });
   },
 };
 
